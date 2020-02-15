2e6072dc108687ae5cdb98b160c365093bba64ac
LeongD <wsld1228@foxmail.com>
2020-02-15 21:37:50 +0800

    init

---
+++ src/settings.js
@@ -0,0 +1,42 @@
+/*
+ * @Description: This file is made for
+ * @Date: 2020-02-15 16:43:27
+ * @LastEditTime: 2020-02-15 21:26:08
+ * @Author: LeongD
+ * @LastEditors: LeongD
+ */
+module.exports = {
+  title: '小慕读书管理后台',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the settings right-panel
+   */
+  showSettings: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether need tagsView
+   */
+  tagsView: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false,
+
+  /**
+   * @type {string | array} 'production' | ['production', 'development']
+   * @description Need show err logs component.
+   * The default is only used in the production env
+   * If you want to also use it in dev, you can pass ['production', 'development']
+   */
+  errorLog: 'production'
+}
